c3c06f7d415acbd2cd0ef1e927b762b854a60ed1
Mark Monserrat <mark.monserrat@me.com>
2018-10-21 05:45:53 +0800

    build 4

--- bundles/redoc.lib.js
+++ bundles/redoc.lib.js
@@ -8585,7 +8585,7 @@ var Schema_Schema = /** @class */ (function (_super) {
             externalDocs: schema.externalDocs,
             deprecated: false,
             toggle: function () { return null; },
-            expanded: false
+            expanded: type === 'object' ? true : false
         }; // cast needed for hot-loader to not fail
         return external_react_["createElement"]("div", null,
             external_react_["createElement"](FieldDetails_FieldDetails, { field: field }));
